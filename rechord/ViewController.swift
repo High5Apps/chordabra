d125a05a94aa7a9bf1101eeb75a5aca2827562d1
High5Apps <high5apps@gmail.com>
2019-11-11 18:31:32 -0800

    Initial Commit

---
+++ rechord/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  rechord
+//
+//  Created by Julian Tigler on 11/11/19.
+//  Copyright © 2019 High5! Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
